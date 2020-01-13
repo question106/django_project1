5a63c4c61b38d88d6b7895e9adf4346dd4cfa1f9
Sanjaa Otgontugs <question106@gmail.com>
2020-01-13 21:35:08 +0900

    Django_Project1

---
+++ Environments/django_proj1_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\sanjaa\django_project1\environments\django_proj1_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
